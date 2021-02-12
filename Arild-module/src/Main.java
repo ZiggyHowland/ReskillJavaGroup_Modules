7684fb0a7aaf7f0d4dd9ebaa37eab88cf6a64e30
marinahaugen <marina.santos.haugen@dnb.no>
2021-02-12 09:38:27 +0100

    Marina tester

--- Arild-module/src/Main.java
+++ Arild-module/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("This is Arild's package");
+        System.out.println("Marina hilser");
+
     }
+
 }
