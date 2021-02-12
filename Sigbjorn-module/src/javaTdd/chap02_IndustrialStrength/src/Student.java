8890fccb7ea872ff1b033af9730d9f8fd8bcaad5
ZiggyHowland <sigbjorn@hovland.nu>
2021-02-12 08:56:39 +0100

    Exercise 1 chapter 2 Industrial Strength

--- Sigbjorn-module/src/javaTdd/chap02_IndustrialStrength/src/Student.java
+++ Sigbjorn-module/src/javaTdd/chap02_IndustrialStrength/src/Student.java
@@ -1,8 +1,44 @@
 package javaTdd.chap02_IndustrialStrength.src;
 
-public class Student {
+import lombok.ToString;
 
-	// TODO.
-	
+@ToString
+public class Student {
+    private String id;
+    private String name;
+    private int availableRoomSlots;
+
+    private static final int MINIMUM_SLOT_AVAILABILITY = 1;
+    private static final int DEFAULT_SLOT_AVAILABILITY = 6;
+
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.availableRoomSlots = DEFAULT_SLOT_AVAILABILITY;
+    }
+
+    public boolean bookSlot() {
+        if (this.availableRoomSlots >= MINIMUM_SLOT_AVAILABILITY) {
+            this.availableRoomSlots--;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean releaseSlot() {
+        if (this.availableRoomSlots < DEFAULT_SLOT_AVAILABILITY) {
+            this.availableRoomSlots++;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int getAvailableRoomSlots() {
+        return availableRoomSlots;
+    }
 }
 
--- Sigbjorn-module/test/javaTdd/chap02_industrialStrength/TestStudent.java
+++ Sigbjorn-module/test/javaTdd/chap02_industrialStrength/TestStudent.java
@@ -1,7 +1,66 @@
 package javaTdd.chap02_industrialStrength;
 
-public class TestStudent {
+import javaTdd.chap02_IndustrialStrength.src.Student;
+//import org.junit.Assert; // => Assert.assertEqual
+import static org.junit.Assert.*; // => assertEqual
+import org.junit.Before;
+import org.junit.Test;
 
-	// Todo - test the Student class.
+public class TestStudent {
+    Student student;
+
+    @Before
+    public void setup() {
+        student = new Student("SH", "Sigbjørn");
+    }
+
+    @Test
+    public void newStudent_beforeUse_shouldHaveSixAvailableSlots() {
+        // **** Q: Should this method name rather refer to DEFAULT instead of Six, and check towards the constant?
+        assertEquals(6, student.getAvailableRoomSlots());
+    }
+
+    @Test
+    public void newStudent_withNoRoomsBooked_shouldBeAllowedToBookRom() {
+        assertTrue(student.bookSlot());
+    }
+
+    @Test
+    public void bookRoomSlot_byOne_shouldReduceAvailableSlotsByOne() {
+        student.bookSlot();
+        assertEquals(5, student.getAvailableRoomSlots());
+    }
+
+    @Test
+    public void bookRoomSlots_withMoreThanOne_shouldReduceAvailability() {
+        student.bookSlot();
+        student.bookSlot();
+        student.bookSlot();
+        student.bookSlot();
+        assertEquals(2, student.getAvailableRoomSlots());
+    }
+
+    @Test
+    public void bookRoomSlots_withTooManyBookings_shouldReturnFalse() {
+        student.bookSlot();
+        student.bookSlot();
+        student.bookSlot();
+        student.bookSlot();
+        student.bookSlot();
+        student.bookSlot();
+        assertFalse(student.bookSlot());
+    }
+
+    @Test
+    public void releaseRoomSlot_withNewStudent_shouldReturnFalse() {
+        assertFalse(student.releaseSlot());
+    }
+
+    @Test
+    public void releaseRoomSlot_afterOneBooking_shouldChangeAvailabilityToSix() {
+        student.bookSlot();
+        student.releaseSlot();
+        assertEquals(6, student.getAvailableRoomSlots());
+    }
 	
 }
