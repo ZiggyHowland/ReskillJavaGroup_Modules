9c859edd7e7f5ec44d9ba4a0fa76e7094ed067d0
ZiggyHowland <sigbjorn@hovland.nu>
2021-02-12 09:38:29 +0100

    La inn en hilsen

--- Arild-module/src/Main.java
+++ Arild-module/src/Main.java
@@ -1,5 +1,18 @@
+
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("This is Arild's package");
+
+
+        System.out.println("Sigbjørn hilser til resten");
+
+
     }
+
+
+
+
+
 }
