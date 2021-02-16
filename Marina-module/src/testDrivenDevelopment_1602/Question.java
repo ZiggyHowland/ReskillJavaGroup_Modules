6356df4d4ef680cfc70cdf36ddd3c04d078d9528
marinahaugen <marina.santos.haugen@dnb.no>
2021-02-16 13:55:19 +0100

    Lab 3. Bugs fixed.

--- Marina-module/src/testDrivenDevelopment_1602/Question.java
+++ Marina-module/src/testDrivenDevelopment_1602/Question.java
@@ -1,15 +1,15 @@
 package testDrivenDevelopment_1602;
 
-
-
 public class Question {
 
-    int questionId;
-    String title;
-    String message;
-    int votes;
-    User user;
-    boolean isOpen = true;
+    private int questionId;
+    private String title;
+    private String message;
+    private int votes;
+    private User user;
+    private boolean isOpen = true;
+
+    private static final long RATING_INCREASE = 10;
 
     public Question(String title, String message, User user) {
         this.title = title;
@@ -19,34 +19,20 @@ public class Question {
 
     public void vote(boolean likeQuestion) {
         if (likeQuestion && isOpen) {
-            // ANDY: Should use static final variable instead of magic number 10.
-            user.rating = user.rating + 10;
+            user.rating = user.rating + RATING_INCREASE;
             votes++;
         }
         // ANDY: You need an "else if" to ensure you don't do any of the rest of the logic if the first "if" statement is true.
         // This explains why you're getting an exception in your test - it's because the 2nd "if" statement fails so it then goes into the "else" part. 
-        if (!likeQuestion && isOpen) {
+        else if (!likeQuestion && isOpen) {
             // ANDY: Ditto
             user.rating = user.rating - 10;
             votes--;
         } else {
             // ANDY: Whoever calls vote() will have to catch it. 
-            // Also note, QuestionException inherits from RuntimeException, which means it's not a "checked" exception, which means the compiler won't enfore that you catch it.
+            // Also note, QuestionException inherits from RuntimeException, which means it's not a "checked" exception, which means the compiler won't enforce that you catch it.
             throw new QuestionException("The question is closed and cannot be voted on.\n"); //Why do we not have to catch this exception
         }
-    }
-    
-    // ANDY: I would actually remove some of these getters/setters.
-    public int getQuestionId() {
-        return questionId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getMessage() {
-        return message;
     }
 
     public int getVotes() {
--- Marina-module/test/testDrivenDevelopment_1602/TestQuestion.java
+++ Marina-module/test/testDrivenDevelopment_1602/TestQuestion.java
@@ -6,18 +6,16 @@ import static org.junit.Assert.assertEquals;
 
 public class TestQuestion {
 
-    // ANDY: This fails because there's a bug in your vote() method. See my comment there. Just as well you found the bug in your test :-)
     @Test
-    public void vote_questionVotedUp_voteIncremented() { //throws exception. "The question is closed and cannot be voted on" - Why?
+    public void vote_questionVotedUp_voteIncremented() {
         Question q = new Question("Math question", "Is 2+2 = 4", new User(1, "Marina", "msh@gmail.com"));
         q.vote(true);
         q.setOpen(true);
         assertEquals(1, q.getVotes());
     }
 
-    // ANDY: Same problem as above.
     @Test
-    public void vote_questionVotedUp_userRatingIncremented() { //throws exception. "The question is closed and cannot be voted on" - Why?
+    public void vote_questionVotedUp_userRatingIncremented() {
         Question q = new Question("Math question", "Is 2+2 = 4", new User(1, "Marina", "msh@gmail.com"));
         q.vote(true);
         assertEquals(10, q.getUser().rating);
